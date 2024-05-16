41ba28355d48a66c6aae4408b4c22299d505008a
Pratik Biswas <04biswaspratik@gmail.com>
2024-05-16 18:23:48 +0530

    nav bar is updated

---
+++ Client/src/components/Nav/Navbar.jsx
@@ -0,0 +1,110 @@
+"use client";
+import React from "react";
+import { Img, Button, Text, Heading, Input } from "../../components";
+import Link from "next/link";
+import { Sidebar, sidebarClasses } from "react-pro-sidebar";
+
+// icons
+import { FaHome, FaUserFriends } from "react-icons/fa";
+import { FaCircleUser, FaFilter } from "react-icons/fa6";
+import { BsSendFill } from "react-icons/bs";
+import { IoIosNotifications, IoIosSettings, IoIosLogOut } from "react-icons/io";
+
+// data for navbar
+export const navData = [
+  { name: "Home", path: "/home1", icon: <FaHome /> },
+  { name: "Profile", path: "/myprofile", icon: <FaCircleUser /> },
+  { name: "Friends", path: "/myfriends", icon: <FaUserFriends /> },
+  { name: "Messages", path: "/messages", icon: <BsSendFill /> },
+  {
+    name: "Notifications",
+    path: "/notifications",
+    icon: <IoIosNotifications />,
+  },
+];
+export const navData1 = [
+  { name: "Settings", path: "/settings", icon: <IoIosSettings /> },
+  { name: "Logout", path: "/", icon: <IoIosLogOut /> },
+];
+
+const Navbar = () => {
+  const [collapsed, setCollapsed] = React.useState(false);
+  //use this function to collapse/expand the sidebar
+  //function collapseSidebar() {
+  //    setCollapsed(!collapsed)
+  //}
+  return (
+    <Sidebar
+      width="100px !important"
+      collapsedWidth="80px !important"
+      collapsed={collapsed}
+      rootStyles={{
+        [`.${sidebarClasses.container}`]: { gap: 0 },
+      }}
+      className="!sticky top-0  h-screen overflow-auto bg-[#cdcdcd] pt-0 md:gap-[1px] sm:gap-[50px] flex lg:flex-col md:flex-row items-center justify-center"
+    >
+      <div className="flex flex-col items-center justify-center pt-11 gap-[50px]">
+        <Img
+          src="img_sidebar_logo.svg"
+          width={48}
+          height={48}
+          alt="sidebarlogo"
+          className="h-[48px] w-[48px] object-contain"
+        />
+
+        <div className="mb-[15px]  flex flex-col gap-80">
+          <div className="flex flex-col  gap-8">
+            {navData.map((link, index) => {
+              return (
+                // target="_blank"
+                <Link href={link.path} key={index} className="mx-2">
+                  <div className="flex flex-col items-center justify-center relative group w-full">
+                    <div className="absolute top-[-32px] hidden group-hover:flex">
+                      <div className="bg-[#000] relative flex text-[#fff] items-center p-[6px] rounded-[3px]">
+                        {/* names */}
+                        <div className="text-[12px] leading-none font-semibold capitalize flex-grow text-center relative">
+                          {link.name}
+                          {/* triangle */}
+                          <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-0 h-0 border-solid border-t-[10px] border-t-black border-x-[8px] border-x-transparent "></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-center py-1 px-3 cursor-pointer rounded-lg hover:bg-black text-2xl text-[#fff]">
+                      {link.icon}
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+
+          <div className="flex flex-col gap-8">
+            {navData1.map((link, index) => {
+              return (
+                <Link href={link.path} key={index} className="mx-2">
+                  <div className="flex flex-col items-center justify-center relative group w-full">
+                    <div className="absolute top-[-32px] hidden group-hover:flex">
+                      <div className="bg-[#000] relative flex text-[#fff] items-center p-[6px] rounded-[3px]">
+                        {/* names */}
+                        <div className="text-[12px] leading-none font-semibold capitalize flex-grow text-center relative">
+                          {link.name}
+                          {/* triangle */}
+                          <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-0 h-0 border-solid border-t-[10px] border-t-black border-x-[8px] border-x-transparent "></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-center py-1 px-3 cursor-pointer rounded-lg hover:bg-black text-2xl text-[#fff]">
+                      {link.icon}
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Sidebar>
+  );
+};
+
+export default Navbar;
