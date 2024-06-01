e3330cd04d4210f9fb911780e8d47e13fc8ecbb6
rupal-draft <rupalpaulgspr@gmail.com>
2024-06-02 03:07:17 +0530

    My profile

--- Client/src/components/Postcard/data.js
+++ Client/src/components/Postcard/data.js
@@ -54,3 +54,55 @@ export const postData = [
     image: "post3.jpg",
   },
 ];
+
+export const data1 = [
+  { imageone: "img_image_221x221.png" },
+  { imageone: "img_image_39.png" },
+  { imageone: "img_image_40.png" },
+  { imageone: "img_image_41.png" },
+  { imageone: "img_image_42.png" },
+  { imageone: "img_image_43.png" },
+  { imageone: "img_image_44.png" },
+  { imageone: "img_image_45.png" },
+];
+export const data2 = [
+  {
+    avatarone: "ed.png",
+    avatarthree: "img_avatar_55.png",
+    avatarfive: "img_avatar_56.png",
+    avatarseven: "img_avatar_57.png",
+  },
+  {
+    avatarone: "img_avatar_59.png",
+    avatarthree: "img_avatar_60.png",
+    avatarfive: "img_avatar_61.png",
+    avatarseven: "img_avatar_62.png",
+  },
+  {
+    avatarone: "img_avatar_64.png",
+    avatarthree: "img_avatar_65.png",
+    avatarfive: "img_avatar_66.png",
+    avatarseven: "img_avatar_67.png",
+  },
+  {
+    avatarone: "img_avatar_64.png",
+    avatarthree: "img_avatar_65.png",
+    avatarfive: "img_avatar_66.png",
+    avatarseven: "img_avatar_67.png",
+  },
+];
+
+export const data = [
+  {
+    title: "Tropical Fresh Tourism Is Back In Full Swing In ",
+    image: "img_image_69x120.png",
+  },
+  {
+    title: "How Timberland created the visual campaign of the ",
+    image: "img_image_26.png",
+  },
+  {
+    title: "Take your mobile photography to the next ",
+    image: "img_image_38.png",
+  },
+];
