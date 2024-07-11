beed25889041b6e231c4314b29920925120ccf6a
Pratik Biswas <04biswaspratik@gmail.com>
2024-07-11 11:34:39 +0530

    my profile page is responsive

--- Client/src/app/myprofile/index.jsx
+++ Client/src/app/myprofile/index.jsx
@@ -11,9 +11,8 @@ import { POSTS_BY_USER_QUERY } from "@/graphql/query";
 import { useQuery } from "@apollo/client";
 import Link from "next/link";
 
-
 export default function MyProfilePage() {
-  const maxDisplayedPhotos = 5;
+  const maxDisplayedPhotos = 8;
   const maxDisplayedFollowers = 8;
 
   const [collapsed, setCollapsed] = React.useState(false);
@@ -52,7 +51,7 @@ export default function MyProfilePage() {
       setPosts(data.postsByUser);
     }
   }, [data]);
-  
+
   useEffect(() => {
     if (user) {
       loadFollowers();
@@ -67,16 +66,17 @@ export default function MyProfilePage() {
   };
 
   return (
-    <div className="flex items-start justify-center gap-5 bg-background">
+    <div className="flex w-full h-full mt-5 sm:mt-0 gap-4 min-h-screen bg-background">
       {/* Nav bar */}
       <Navbar open={open} setOpen={setOpen} />
+
       <div
-        className={`md:hidden fixed z-50 bottom-0 transition-all duration-700 ${
-          open ? "left-[4.5rem] px-2 py-1" : "left-0 p-1"
+        className={`lg:hidden fixed z-50 bottom-0 transition-all duration-700 ${
+          open ? "left-[6rem] px-2 py-1" : "left-0 p-1"
         }`}
       >
         <h1
-          className="text-2xl bg-gray-50 p-2 rounded-xl font-semibold transition-transform duration-700"
+          className="text-2xl bg-highlight text-shadow p-2 rounded-lg font-semibold transition-transform duration-700"
           onClick={() => {
             setOpen(!open);
           }}
@@ -85,50 +85,62 @@ export default function MyProfilePage() {
         </h1>
       </div>
 
-      <div className="flex items-center justify-center gap-5 md:w-full flex-col md:pt-7 pb-4 mr-5">
+      <div className="flex items-center justify-center gap-5 md:w-full flex-col md:pt-7 pb-4 mr-5 ml-5 lg:ml-0">
         <div className="mt-0 flex flex-col gap-10">
           {/* rest contents */}
 
           <div className="flex flex-col items-start justify-center gap-y-5">
             {/* upper part */}
 
-            <div className="flex flex-col items-start gap-y-10 rounded-xl p-[18px] bg-shadow">
+            <div className="flex flex-col items-center gap-y-10 rounded-xl p-[18px] bg-shadow">
               {/* 0th part */}
-              <div className="flex relative flex-col items-center justify-center">
+              <div className="flex flex-col items-center justify-center">
                 {isClient && user?.coverphoto?.url ? (
                   <img
                     src={user.coverphoto.url}
                     alt="Cover Photo"
                     className="rounded-xl object-cover
                     
-                    w-[90rem] h-[20rem]"
+                   w-[17rem] sm:w-[35rem] md:w-[45rem] lg:w-[50rem] xl:w-[70rem] 2xl:w-[85rem] 
+                  h-[10rem] sm:h-[15rem]  lg:h-[20rem]"
                   />
                 ) : (
                   <div
                     className=" rounded-xl object-cover bg-gray-300 flex items-center justify-center
                   
-                  w-[86rem] h-[20rem]"
+                  w-[17rem] sm:w-[35rem] md:w-[45rem] lg:w-[50rem] xl:w-[70rem] 2xl:w-[85rem] 
+            h-[10rem] sm:h-[15rem]  lg:h-[20rem]"
                   >
                     <span>Cover Photo</span>
                   </div>
                 )}
 
-                <div className="flex absolute top-[13.5rem] left-0 right-0 flex-row items-start justify-center self-stretch gap-5">
+                <div
+                  className="flex absolute left-0 right-0 flex-col items-center gap-1 
+                
+                top-[8.5rem] sm:top-[12.5rem] md:top-[12.5rem] lg:top-[17.5rem] 
+                sm:mx-[12rem]"
+                >
                   {/* profile photo */}
                   <div className="flex flex-col items-center justify-center self-stretch">
-                    <div>
+                    <div
+                      className="rounded-full overflow-hidden border-4 sm:border-8 border-shadow object-cover
+              
+              w-[6rem] sm:w-[8rem] md:w-[12rem] 
+              h-[6rem] sm:h-[8rem] md:h-[12rem] "
+                    >
                       {isClient && user.photo?.url ? (
                         <img
                           src={user.photo.url}
                           alt="Meow"
-                          className="rounded-full w-[12rem] h-[12rem] border-8 border-shadow object-cover"
+                          className="w-full h-full object-cover"
                         />
                       ) : (
                         <Avatar
                           name={isClient && user?.name ? user.name : "User"}
                           size="192"
                           round={true}
-                          className="cursor-pointer rounded-full w-[12rem] h-[12rem] border-2 border-shadow"
+                          className="cursor-pointer rounded-full w-full h-full object-cover border-2 border-shadow"
                           style={{ border: "0px" }}
                         />
                       )}
@@ -165,10 +177,11 @@ export default function MyProfilePage() {
                   </div>
                 </div>
               </div>
+
               {/* 1st part */}
               <div>
                 {/* About me */}
-                <div className="flex flex-col self-stretch gap-y-5 pt-[12rem]">
+                <div className="flex flex-col self-stretch gap-y-5 pt-[8rem] sm:pt-[9rem] md:pt-[12rem]">
                   <h1 className="uppercase text-2xl font-extrabold tracking-[1.00px] text-primary_text">
                     About Me
                   </h1>
@@ -232,7 +245,7 @@ export default function MyProfilePage() {
                 </h1>{" "}
                 <div className="flex flex-col items-start justify-center gap-[30px] rounded-xl">
                   <div className="flex flex-col items-start gap-[15px] self-stretch">
-                    <div className="flex flex-wrap gap-[15px] self-stretch md:grid-cols-3 sm:grid-cols-1 text-primary_text">
+                    <div className="flex flex-wrap gap-[15px] self-stretch grid-cols-3 sm:grid-cols-1 text-primary_text">
                       {posts && posts.length > 0 ? (
                         posts
                           .slice(0, maxDisplayedPhotos)
@@ -243,7 +256,10 @@ export default function MyProfilePage() {
                               width={130}
                               height={130}
                               alt="image"
-                              className="h-[15rem] w-[265px] rounded-xl object-cover"
+                              className=" rounded-xl object-cover
+                              
+                              w-[240px] h-[240px]   min-[360px]:w-[360px] min-[360px]:h-[360px]   sm:w-[240px] sm:h-[240px]   md:w-[220px] md:h-[220px]   lg:w-[208px] lg:h-[208px] xl:w-[272px] xl:h-[272px] min-[1440px]:w-[312px]  min-[1440px]:h-[312px] 2xl:w-[336px] 2xl:h-[336px]
+                              "
                             />
                           ))
                       ) : (
@@ -262,20 +278,12 @@ export default function MyProfilePage() {
                 </div>
               </div>
             </div>
-         
-
 
             {/* right */}
             <div className="grid grid-cols-1 xl:grid-cols-2 gap-[30px] w-full">
               {posts &&
-                posts.map((post, index) => (
-                  <Post
-                    post={post}
-                    key={index}
-                  />
-                ))}
+                posts.map((post, index) => <Post post={post} key={index} />)}
             </div>
-
           </div>
         </div>
       </div>
