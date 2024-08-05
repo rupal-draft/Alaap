418c797f09feb605164b4e588698a97e20d75104
Pratik Biswas <04biswaspratik@gmail.com>
2024-08-05 11:47:55 +0530

    ai chat landing page is final

--- Client/src/app/sociofy-ai/index.jsx
+++ Client/src/app/sociofy-ai/index.jsx
@@ -7,12 +7,12 @@ const HomePage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <img src="ai-robot.png" className="h-72 w-72" />
 
-      <h1 className="text-4xl font-bold mb-8 text-primary_text">
+      <h1 className="text-4xl font-bold mb-8 text-center text-primary_text">
         Choose an AI Service
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <Link href="/ai-chat">
-          <div className="cursor-pointer p-4 border border-hover_accent duration-500 rounded-lg shadow-md shadow-hover_accent hover:shadow-lg transition-shadow">
+          <div className="cursor-pointer  w-[20rem] sm:w-full p-4 border border-hover_accent duration-500 rounded-lg shadow-md shadow-hover_accent hover:shadow-lg transition-shadow">
             <h2 className="text-2xl font-bold mb-2 text-primary_text">
               AI Chat
             </h2>
@@ -22,7 +22,7 @@ const HomePage = () => {
           </div>
         </Link>
         <Link href="/ai-image">
-          <div className="cursor-pointer p-4 border border-hover_accent duration-500 rounded-lg shadow-md shadow-hover_accent hover:shadow-lg transition-shadow">
+          <div className="cursor-pointer  w-[20rem] sm:w-full p-4 border border-hover_accent duration-500 rounded-lg shadow-md shadow-hover_accent hover:shadow-lg transition-shadow">
             <h2 className="text-2xl font-bold mb-2 text-primary_text">
               AI Image Generation
             </h2>
